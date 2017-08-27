5dfb5d2cf00187bc85a98a4701a2387d21c38f4e
foogunlana <foogunlana@yahoo.com>
2017-08-27 15:20:12 +0100

    added deadline and destroy override

--- contracts/Remittance.sol
+++ contracts/Remittance.sol
@@ -5,16 +5,24 @@ import { OwnedDestroyable } from './OwnedDestroyable.sol';
 contract Remittance is OwnedDestroyable{
   bytes32 private passwordHash1;
   bytes32 private passwordHash2;
+  uint public deadline;
+
+  modifier deadlinePassed {
+    assert(block.number >= deadline);
+    _;
+  }
 
   event LogWithdrawal(address indexed _sender);
 
   function () payable {}
 
-  function Remittance(bytes32 _passwordHash1, bytes32 _passwordHash2)
+  function Remittance(
+    bytes32 _passwordHash1, bytes32 _passwordHash2, uint _duration)
     public
     payable {
     passwordHash1 = sha3(_passwordHash1);
     passwordHash2 = sha3(_passwordHash2);
+    deadline = block.number + duration;
   }
 
   function withdraw(bytes32 _passwordHash1, bytes32 _passwordHash2)
@@ -25,5 +33,11 @@ contract Remittance is OwnedDestroyable{
     msg.sender.transfer(this.balance);
     LogWithdrawal(msg.sender);
     return true;
+  }
+
+  function destroy()
+    public
+    deadlinePassed {
+    super.destroy();
   }
 }
