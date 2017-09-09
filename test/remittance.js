fe3bf8c74dedb4ca3fa5e346693c5f4439ff329b
Foluso Ogunlana <folusoogunlana@folusos-air.home>
2017-09-09 20:08:08 +0100

    refactored tests to check that hash is sent and then verified from password and address hash

--- test/remittance.js
+++ test/remittance.js
@@ -52,6 +52,25 @@ function expectedExceptionPromise(action, gasToUse) {
   });
 };
 
+function solSha3 (...args) {
+  args = args.map(arg => {
+    if (typeof arg === 'string') {
+      if (arg.substring(0, 2) === '0x') {
+        return arg.slice(2);
+      } else {
+        return web3.toHex(arg).slice(2);
+      }
+    }
+    if (typeof arg === 'number') {
+      return leftPad((arg).toString(16), 64, 0);
+    } else {
+      return '';
+    }
+  });
+  args = args.join('');
+  return web3.sha3(args, { encoding: 'hex' });
+}
+
 // https://stackoverflow.com/questions/32536049/do-i-need-to-return-after-early-resolve-reject
 // See above for inspiration
 const promisify = function(inner) {
@@ -74,13 +93,12 @@ contract('Remittance', accounts => {
   const amount = web3.toWei(5, 'ether');
   const duration = 10;
   const deadlineTooLong = 100;
-  var contractInstance;
+  let contractInstance;
 
   beforeEach(() => {
     return Remittance.new(
       sender,
-      web3.sha3(password, {encoding: 'hex'}),
-      web3.sha3(recipient, {encoding: 'hex'}),
+      solSha3(password, solSha3(recipient)),
       0,
       {from: owner})
     .then(instance => {
@@ -91,7 +109,7 @@ contract('Remittance', accounts => {
   });
 
   it('should be owned', () => {
-    return contractInstance.owner()
+    return contractInstance.owner.call()
     .then(_owner => {
       assert.equal(
         _owner,
@@ -103,12 +121,11 @@ contract('Remittance', accounts => {
   it('should be destroyable immediately without funds', () => {
     return Remittance.new(
       sender,
-      web3.sha3(password, {encoding: 'hex'}),
-      web3.sha3(recipient, {encoding: 'hex'}),
+      solSha3(password, solSha3(recipient)),
       0,
       {from: owner})
-    .then(instance => {
-      contractInstance = instance;
+    .then(_instance => {
+      contractInstance = _instance;
       return contractInstance.destroy({from: owner});
     })
     .then(txObj => {
@@ -119,119 +136,100 @@ contract('Remittance', accounts => {
     });
   });
 
-  it('should reject deadlines that are too long', () => {
-    return expectedExceptionPromise(() => {
+  describe('Handles the deadline correctly', () => {
+    it('should reject deadlines that are too long', () => {
+      return expectedExceptionPromise(() => {
+        return Remittance.new(
+          sender,
+          solSha3(password, solSha3(recipient)),
+          deadlineTooLong,
+          {from: owner, gas: 1000000})
+        .then(txObj => txObj.tx);
+      }, 1000000);
+    });
+
+    it('should be not be refundable before deadline', () => {
+      var instance;
       return Remittance.new(
         sender,
-        web3.sha3(password, {encoding: 'hex'}),
-        web3.sha3(recipient, {encoding: 'hex'}),
-        deadlineTooLong,
-        {from: owner, gas: 1000000})
-      .then(txObj => txObj.tx);
-    }, 1000000);
-  });
-
-  it('should be not be refundable before deadline', () => {
-    var instance;
-    return Remittance.new(
-      sender,
-      web3.sha3(password, {encoding: 'hex'}),
-      web3.sha3(recipient, {encoding: 'hex'}),
-      duration,
-      {from: owner})
-    .then(_instance => {
-      instance = _instance;
-      return web3.eth.sendTransaction(
-        {from: alice, value: amount, to: Remittance.address});
-    })
-    .then(tx => {
-      return expectedExceptionPromise(() => {
-        return instance.refund({from: alice, gas: 1000000})
-        .then(txObj => txObj.tx);
-      }, 1000000);
+        solSha3(password, solSha3(recipient)),
+        duration,
+        {from: owner})
+      .then(_instance => {
+        instance = _instance;
+        return web3.eth.sendTransaction(
+          {from: alice, value: amount, to: Remittance.address});
+      })
+      .then(tx => {
+        return expectedExceptionPromise(() => {
+          return instance.refund({from: alice, gas: 1000000})
+          .then(txObj => txObj.tx);
+        }, 1000000);
+      });
     });
   });
 
-  it('should release funds to recipient if both correct hashes are provided', () => {
-    var initialBalance;
+  describe("Releases funds if passwords are provided by recipient only", () => {
+    it('should release funds to recipient if passwords are correct', () => {
+      var initialBalance;
+      return promisify((cb) => web3.eth.getBalance(bob, cb))
+      .then(balance => {
+        initialBalance = balance;
+        return contractInstance.withdraw(
+          password,
+          solSha3(recipient),
+          {from: bob});
+      })
+      .then(txObj => {
+        return web3.eth.getBalance(bob);
+      })
+      .then(balance => {
+        assert.isAbove(
+          balance.toNumber(),
+          initialBalance.toNumber(),
+          "Bob's balance wasn't credited!")
+      });
+    });
 
-    return promisify((cb) => web3.eth.getBalance(bob, cb))
-    .then(balance => {
-      initialBalance = balance;
-      return contractInstance.withdraw(
-        web3.sha3(password, {encoding: 'hex'}),
-        web3.sha3(recipient, {encoding: 'hex'}),
-        {from: bob});
-    })
-    .then(txObj => {
-      return web3.eth.getBalance(bob);
-    })
-    .then(balance => {
-      assert.isAbove(
-        balance.toNumber(),
-        initialBalance.toNumber(),
-        "Bob's balance wasn't credited!")
+    it("should not release funds to any addres that isn't the recipient", () => {
+      var initialBalance;
+      var notRecipient = alice;
+      return promisify((cb) => web3.eth.getBalance(bob, cb))
+      .then(balance => {
+        initialBalance = balance;
+        return expectedExceptionPromise(() => {
+          return contractInstance.withdraw(
+            password,
+            recipient,
+            {from: notRecipient, gas: 1000000})
+          .then(txObj => txObj.tx);
+        }, 1000000);
+      })
+    });
+
+    it('should not release funds to any account without correct passwords', () => {
+      var initialBalance;
+      return promisify((cb) => web3.eth.getBalance(bob, cb))
+      .then(balance => {
+        initialBalance = balance;
+        return expectedExceptionPromise(() => {
+          return contractInstance.withdraw(
+            wrongPassword,
+            recipient,
+            {from: bob, gas: 1000000})
+          .then(txObj => txObj.tx);
+        }, 1000000);
+      });
     });
   });
 
-  it('should not release funds to any account without correct passwords', () => {
-    var initialBalance;
-
-    return promisify((cb) => web3.eth.getBalance(bob, cb))
-    .then(balance => {
-      initialBalance = balance;
-      return expectedExceptionPromise(() => {
-        return contractInstance.withdraw(
-          web3.sha3(wrongPassword, {encoding: 'hex'}),
-          web3.sha3(recipient, {encoding: 'hex'}),
-          {from: bob, gas: 1000000})
-        .then(txObj => txObj.tx);
-      }, 1000000);
-    })
-  });
-
-  it("should not release funds to any addres that isn't the recipient", () => {
-    var initialBalance;
-    var notRecipient = alice;
-    return promisify((cb) => web3.eth.getBalance(bob, cb))
-    .then(balance => {
-      initialBalance = balance;
-      return expectedExceptionPromise(() => {
-        return contractInstance.withdraw(
-          web3.sha3(password, {encoding: 'hex'}),
-          web3.sha3(recipient, {encoding: 'hex'}),
-          {from: notRecipient, gas: 1000000})
-        .then(txObj => txObj.tx);
-      }, 1000000);
-    })
-  });
-
-  it('it should give some commission to the owner upon withdrawal', () => {
-    var initialBalance;
-    return Remittance.new(
-      sender,
-      web3.sha3(password, {encoding: 'hex'}),
-      web3.sha3(recipient, {encoding: 'hex'}),
-      0,
-      {from: owner})
-    .then(instance => {
-      contractInstance = instance;
-      return web3.eth.getBalance(owner);
-    })
-    .then(_balance => {
-      initialBalance = _balance;
-      var txn = web3.eth.sendTransaction(
-        {from: alice, value: amount, to: contractInstance.address});
-      return getTransactionReceiptMined(txn);
-    })
-    .then(receipt => {
-      return web3.eth.getBalance(owner);
-    })
-    .then(balance => {
+  it('it should save some commission for the owner upon withdrawal', () => {
+    return contractInstance.commission.call()
+    .then(commission => {
       assert.isAbove(
-        balance.toNumber(),
-        initialBalance.toNumber(),
-        "Owner's balance wasn't credited!")
+        commission.toNumber(),
+        0,
+        "Owner's commission wasn't allocated!")
     });
   });
 });
